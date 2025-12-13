8047e697ccc9fcaa551cebd8057e14af3e3ff8cd
something1703 <rvsrathore17@gmail.com>
2025-12-13 18:54:35 +0530

    fix: make GITHUB_TOKEN optional for Vercel deployment

--- lib/github.ts
+++ lib/github.ts
@@ -3,7 +3,9 @@ import axios, { AxiosInstance } from "axios";
 const github: AxiosInstance = axios.create({
   baseURL: "https://api.github.com",
   headers: {
-    Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+    ...(process.env.GITHUB_TOKEN && {
+      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+    }),
     "User-Agent": "r2d2-agent"
   }
 });
